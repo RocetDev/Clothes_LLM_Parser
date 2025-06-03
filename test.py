77ab84fe23f6ca9adc115779a35e11329715df5c
RocetDev <curyk39@gmail.com>
2025-06-03 14:42:53 +0500

    Add requarements file

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ URL = 'https://choux.ru/product/polo-iz-hlopka-ping-pong-games-shokoladnoe' # fo
 
 
 parser_obj = parserinfo.create_parser_object(
-    api_key='<YOUR OPENROUTER API KEY>'
+    api_key='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'
 )
 
 with open('product_info.json', 'w', encoding='utf-8') as file:
@@ -15,7 +15,7 @@ with open('product_info.json', 'w', encoding='utf-8') as file:
     print('[INFO] Parsing ends')
 
     print("[INFO] Parsing the product from page...")
-    product_info = parserinfo.parse_info(page, parser_obj, model='meta-llama/llama-3.3-70b-instruct:free')
+    product_info = parserinfo.parse_info(page, parser_obj) # , model='meta-llama/llama-3.3-70b-instruct:free'
     json.dump(product_info, file)
 
     print('[INFO] File created!')
